3c2e9fc39369aed97166f472ef5068e51a7a718f
Thiago Garcia <thiagoufg@gmail.com>
2018-02-12 21:54:51 -0200

    Aula 2 Starter DATA JPA

--- src/main/java/br/com/alura/listavip/Configuracao.java
+++ src/main/java/br/com/alura/listavip/Configuracao.java
@@ -1,24 +1,29 @@
 package br.com.alura.listavip;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
-@Controller
 public class Configuracao {
-
-	@RequestMapping("/")
-	@ResponseBody
-	String ola() {
-		return "Olá. Está tudo funcionando";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Configuracao.class, args);
 	}
+	
+	@Bean
+	public DataSource dataSource(){
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/casadocodigo");
+		dataSource.setUsername("thiago");
+		dataSource.setPassword("123456");
+		return dataSource;
+	}
+	
+	
 
 }
